52641140c829c437a9000536bdef949d35f1d5a5
preksha-23-dp <150505354+preksha-23-dp@users.noreply.github.com>
2025-11-07 15:12:55 +0530

    Add files via upload

---
+++ InteractiveNavProject/script.js
@@ -0,0 +1,9 @@
+// When user scrolls, add or remove "scrolled" class to navbar
+window.addEventListener("scroll", () => {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
